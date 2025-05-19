fd1efcc879f17bbff565ad8e7a9392086e3d4a3b
sir-serge <irkzsrg@gmail.com>
2025-05-19 23:35:19 +0200

    using railway configiration

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=pharmacydb", "root", "");
+    $conn = new PDO("mysql:host=hopper.proxy.rlwy.net;port=25604;dbname=railway","bAcBhBEsJARRCPScSlaUXXZZsoiBUxIV");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     error_log("Connection failed: " . $e->getMessage());
