ecd0788f903213a47915a096f545365832ff73ca
sir-serge <irkzsrg@gmail.com>
2025-05-19 23:53:48 +0200

    last updates

--- config/db.php
+++ config/db.php
@@ -1,9 +1,17 @@
 <?php
+
+$host = getenv('MYSQL_HOST');
+$dbname = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$port = getenv('MYSQLPORT'); // If a port is explicitly provided
 try {
-    $conn = new PDO("mysql:host=hopper.proxy.rlwy.net;port=25604;dbname=railway","bAcBhBEsJARRCPScSlaUXXZZsoiBUxIV");
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     error_log("Connection failed: " . $e->getMessage());
     die("Connection failed: " . $e->getMessage());
 }
 ?>
+
+
